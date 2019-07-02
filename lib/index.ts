c8fe40b57f78a2b52776c8490e3f6cd3618fc4bb
Andrew Wittrock <andy@wittrock.us>
2019-07-02 12:55:23 -0500

    Implement some functions

---
+++ lib/index.ts
@@ -0,0 +1,98 @@
+// Common
+export const reduce = <A, B>(r:Reducer<A, B>, def:B):Func<A[], B> => (arr:A[]):B => arr.reduce(r, def);
+export const as = <T, U>(obj:T, f:Func<T, U>) => f(obj);
+export const prop = <T, U extends keyof T>(prop:U) => (obj:T):T[U] => obj[prop];
+export const tuple = {
+    first: <A, B>(t:Tuple<A, B>):A => t[0],
+    second: <A, B>(t:Tuple<A, B>):B => t[1],
+};
+
+
+// Array functions
+export const at = <T>(index:number):Func<T[], Maybe<T>> => (arr:T[]):Maybe<T> => arr[index];
+export const concat = <T>(item:T | T[]):Func<T[], T[]> => (arr:T[]):T[] => ([] as T[]).concat(arr).concat(item);
+export const createIndex = <T, U>(getId:Func<T, string>, transform:Func<T, U>):Func<T[], Index<U>> => (arr:T[]):Index<U> => arr.reduce(
+    (acc:Index<U>, cur:T):Index<U> => ({
+        ...acc,
+        [getId(cur)]: transform(cur)
+    }),
+    {}
+);
+export const entries = <T>(arr:T[]):IterableIterator<[number, T]> => arr.entries();
+export const every = <T>(f:Func<T, boolean>):Func<T[], boolean> => (arr:T[]):boolean => arr.every(f);
+export const filter = <T>(f:Func<T, boolean>):Func<T[], T[]> => (arr:T[]):T[] => arr.filter(f);
+export const find = <T>(f:Func<T, boolean>):Func<T[], Maybe<T>> => (arr:T[]):Maybe<T> => arr.find(f);
+export const findIndex = <T>(f:Func<T, boolean>):Func<T[], number> => (arr:T[]):number => arr.findIndex(f);
+export const first = at(0);
+export const flatten = <T>(arr:T[][]):T[] => reduce((acc:T[], cur:T[]):T[] => ([] as T[]).concat(acc).concat(cur), [])(arr);
+export const from = concat([]);
+export const includes = <T>(item:T):Func<T[], boolean> => (arr:T[]):boolean => arr.includes(item);
+export const indexOf = <T>(item:T, start?:number):Func<T[], number> => (arr:T[]):number => arr.indexOf(item, start);
+export const joinWith = <T>(sep:string):Func<T[], string> => (arr:T[]):string => arr.join(sep);
+// TODO: juxt
+export const keys = <T>(arr:T[]):IterableIterator<number> => arr.keys();
+export const last = <T>(arr:T[]):Maybe<T> => at<T>(arr.length - 1)(arr);
+export const lastIndexOf = <T>(item:T):Func<T[], number> => (arr:T[]):number => arr.lastIndexOf(item);
+export const map = <A, B>(m:Func<A, B>):Func<A[], B[]> => (arr:A[]):B[] => arr.map(m);
+export const partition = <T>(getId:Func<T, string>):Func<T[], IHash<T>> => (arr:T[]):IHash<T> => arr.reduce(
+    (acc:IHash<T>, cur:T):IHash<T> => as(getId(cur), (key:string) => ({
+        ...acc,
+        [getId(cur)]: acc[key] ? concat(cur)(acc[key]) : [cur]
+    })),
+    {}
+);
+export const partitionOn = <T>(propName:string) => partition<T>(prop<any, any>(propName));
+export const pop = <T>(arr:T[]):Tuple<Maybe<T>, T[]> => {
+    let a:T[] = concat([] as T[])(arr);
+    const elem:Maybe<T> = a.pop();
+    return [elem, a];
+}
+export const push = <T>(item:T):Func<T[], T[]> => (arr:T[]):T[] => {
+    let a:T[] = concat([] as T[])(arr);
+    a.push(item);
+    return a;
+}
+export const range = (start:number, end:number):number[] =>
+    Array(Math.abs(end - start) + 1)
+    .map((_:number, i:number) => start < end ? i + start : start - i);
+
+export const reverse = <T>(arr:T[]):T[] => ([] as T[]).concat(arr).reverse();
+export const shift = <T>(arr:T[]):Tuple<Maybe<T>, T[]> => {
+    let a:T[] = concat([] as T[])(arr);
+    const elem:Maybe<T> = a.shift();
+    return [elem, a];
+}
+export const unshift = <T>(item:T):Func<T[], T[]> => (arr:T[]):T[] => {
+    let a:T[] = concat([] as T[])(arr);
+    a.unshift(item);
+    return a;
+}
+export const slice = <T>(start?:number, end?:number):Func<T[], T[]> => (arr:T[]):T[] => arr.slice(start, end);
+export const splice = <T>(start:number, length?:number) => (arr:T[]):T[] => {
+    let newArr:T[] = [...arr];
+    newArr.splice(start, length);
+    return newArr;
+};
+export const some = <T>(f:Func<T, boolean>):Func<T[], boolean> => (arr:T[]):boolean => arr.some(f);
+export const sort = <T>(f:Func<T, number>):Func<T[], T[]> => (arr:T[]):T[] => concat([] as T[])(arr).sort(f);
+export const unique = <T>(arr:T[]):T[] => [...new Set(arr)];
+
+// Object methods
+export const clone = <T>(obj:T):T => Object.assign({}, obj);
+
+// Helpers
+export const debug = <T>(obj:T):T => {console.log(obj); return obj;}
+export const identity = <T>(obj:T):T => obj;
+export const get = <T>(obj:T):Func<undefined, T> => () => obj;
+
+// Function composition
+export const compose:ICompose = <A, B>(...funcs:Func<any, any>[]):Func<A, B> => (obj:A):B => reverse(funcs).reduce(
+    (acc:any, cur:any) => cur(acc),
+    obj
+);
+
+export const pipe:IPipe = <A, B>(...funcs:Func<any, any>[]):Func<A, B> => (obj:A):B => funcs.reduce(
+    (acc:any, cur:any) => cur(acc),
+    obj
+);
+
---
+++ lib/types.d.ts
@@ -0,0 +1,46 @@
+type Func<A, B> = (obj:A) => B;
+type Reducer<A, B> = (acc:B, cur:A) => B;
+interface Index<T> {
+    [key:string]:T;
+}
+interface IHash<T> {
+    [key:string]:T[];
+}
+type Maybe<T> = T | undefined;
+type Tuple<A, B> = [A, B];
+
+interface ICompose {
+    <A, T2, B>(f2:Func<T2, B>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, B>(f3:Func<T3, B>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, B>(f4:Func<T4, B>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, B>(f5:Func<T5, B>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, B>(f6:Func<T6, B>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, B>(f7:Func<T7, B>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, B>(f8:Func<T8, B>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, B>(f9:Func<T9, B>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, B>(f10:Func<T10, B>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, B>(f11:Func<T11, B>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, B>(f12:Func<T12, B>, f11:Func<T11, T12>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, B>(f13:Func<T13, B>, f12:Func<T12, T13>, f11:Func<T11, T12>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, B>(f14:Func<T14, B>, f13:Func<T13, T14>, f12:Func<T12, T13>, f11:Func<T11, T12>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, B>(f15:Func<T15, B>, f14:Func<T14, T15>, f13:Func<T13, T14>, f12:Func<T12, T13>, f11:Func<T11, T12>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, B>(f16:Func<T16, B>, f15:Func<T15, T16>, f14:Func<T14, T15>, f13:Func<T13, T14>, f12:Func<T12, T13>, f11:Func<T11, T12>, f10:Func<T10, T11>, f9:Func<T9, T10>, f8:Func<T8, T9>, f7:Func<T7, T8>, f6:Func<T6, T7>, f5:Func<T5, T6>, f4:Func<T4, T5>, f3:Func<T3, T4>, f2:Func<T2, T3>, f1:Func<A, T2>):Func<A, B>;
+}
+
+interface IPipe {
+    <A, T2, B>(f1:Func<A, T2>, f2:Func<T2, B>):Func<A, B>;
+    <A, T2, T3, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, B>):Func<A, B>;
+    <A, T2, T3, T4, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, T12>, f12:Func<T12, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, T12>, f12:Func<T12, T13>, f13:Func<T13, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, T12>, f12:Func<T12, T13>, f13:Func<T13, T14>, f14:Func<T14, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, T12>, f12:Func<T12, T13>, f13:Func<T13, T14>, f14:Func<T14, T15>, f15:Func<T15, B>):Func<A, B>;
+    <A, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, B>(f1:Func<A, T2>, f2:Func<T2, T3>, f3:Func<T3, T4>, f4:Func<T4, T5>, f5:Func<T5, T6>, f6:Func<T6, T7>, f7:Func<T7, T8>, f8:Func<T8, T9>, f9:Func<T9, T10>, f10:Func<T10, T11>, f11:Func<T11, T12>, f12:Func<T12, T13>, f13:Func<T13, T14>, f14:Func<T14, T15>, f15:Func<T15, T16>, f16:Func<T16, B>):Func<A, B>;
+}
