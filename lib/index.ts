432d8b982acf14322ddf51204d1bcc1564c00ca0
Andrew Wittrock <andy@wittrock.us>
2021-11-30 11:35:29 -0600

    Use inspect instead of JSON.stringify for hashing in order to support circular objects

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 import typeOf from 'type-of';
+import {inspect} from 'util';
 import { Func, ICompose, IHash, IJuxt, IMemoizeOptions, Index, IPipe, ISwitch, Maybe, MaybeNull, Reducer, SyncOrAsync, Tuple, Variadic, MatchFunc } from './types';
 
 // Common
@@ -167,7 +168,7 @@ export const append = (suffix:string):Func<string, string> => (str:string):strin
 export const charAt = (index:number):Func<string, string> => (str:string) => str.charAt(index);
 export const charCodeAt = (index:number):Func<string, number> => (str:string):number => str.charCodeAt(index);
 export const endsWith = (suffix:string):Func<string, boolean> => (str:string):boolean => str.endsWith(suffix);
-export const hash = (str:string):string => JSON.stringify(str);
+export const hash = (str:string):string => inspect(str);
 export const matches = (regex:RegExp):Func<string, MaybeNull<string[]>> => (str:string):MaybeNull<string[]> => str.match(regex);
 export const prepend = (prefix:string):Func<string, string> => (str:string):string => `${prefix}${str}`;
 export const repeat = (count:number):Func<string, string> => (str:string):string => str.repeat(count);
@@ -190,7 +191,7 @@ export const isNotEmpty = <T>(obj:T):boolean => obj ? true : false;
 export const debug = <T>(obj:T):T => {console.log(obj); return obj;}
 export const identity = <T>(obj:T):T => obj;
 export const get = <T>(obj:T):Func<undefined, T> => ():T => obj;
-export const stringify = <T>(obj:T):string => JSON.stringify(obj);
+export const stringify = <T>(obj:T):string => inspect(obj);
 export const arg = <T>(index:number):((...args:any[]) => T) => (...args:any[]) => args[index] as T;
 export const args = <T>(...indices:number[]):((...args:any[]) => T) => (...args:any[]):T => indices.map(i => args[i]) as unknown as T;
 
